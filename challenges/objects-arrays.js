3ddb1fb48b7d67eed84b1deabca669f84cfd518e
Melissa Jones <mjhome@Melissas-MacBook-Pro.local>
2019-06-14 11:57:50 -0400

    START initial commit. created dinosaur objects in arraymethods.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,6 +12,13 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+//dinosaur objects
+const dinos = [{name: "tyrannosaurus", diet: "carnivorous", weight: "7000kg", length: "12m", period: "Late Cretaceous"},
+{name: "stegosaurus", diet: "herbivorous", weight: "2000kg", length: "9m", period: "Late Jurrasic"},
+{name: "velociraptor", diet: "carnivorous", weight: "15kg", length: "1.8m", period: "Late Cretaceous"}]
+
+console.log(dinos);
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
